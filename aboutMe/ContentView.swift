b540a3e189062adbe7bf98059f39c0a6ac596b73
Sophie Patel <sophie.e.k.patel@gmail.com>
2023-08-01 09:31:27 +0100

    Added comment

--- aboutMe/ContentView.swift
+++ aboutMe/ContentView.swift
@@ -7,15 +7,39 @@
 
 import SwiftUI
 
+//COMMENT
+
 struct ContentView: View {
+    @State private var isClicked = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("mathsCat")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(40)
+            
+            Text("Sophie")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(Color(hue: 0.086, saturation: 0.417, brightness: 0.717))
+                .padding(/*@START_MENU_TOKEN@*/[.leading, .bottom, .trailing]/*@END_MENU_TOKEN@*/)
+            Button("Click for fun facts!") {
+                isClicked = true
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.brown)
+            if isClicked == true {
+                Text("I love science")
+                    .foregroundColor(Color(red: 0.717, green: 0.571, blue: 0.419))
+                Text("I have a cat called Whisper")
+                    .foregroundColor(Color(red: 0.717, green: 0.571, blue: 0.419))
+                Text("I am learning how to crochet")
+                    .foregroundColor(Color(red: 0.717, green: 0.571, blue: 0.419))
+            }
+            
         }
-        .padding()
+        .padding(.all)
     }
 }
 
